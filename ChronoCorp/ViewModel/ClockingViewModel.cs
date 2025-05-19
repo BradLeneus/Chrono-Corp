8e98779e50a84d4e324ff968d308e57441143852
911 <167906072+usernametarek@users.noreply.github.com>
2025-05-18 20:18:50 -0400

    ClockingViewModel.cs : pointage fonction implémentée : VERSION 1.0.0

--- ChronoCorp/ViewModel/ClockingViewModel.cs
+++ ChronoCorp/ViewModel/ClockingViewModel.cs
@@ -1,6 +1,7 @@
-﻿using ChronoCorp.Interface;
+using ChronoCorp.Interface;
 using ChronoCorp.Model;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -20,18 +21,79 @@ namespace ChronoCorp.ViewModel
         [ObservableProperty]
         private ObservableCollection<CeduleQuart> shiftToClockList = new();
 
+        [ObservableProperty]
+        private DateTime? newHeureDebut;
+
+        [ObservableProperty]
+        private DateTime? newHeureFin;
+
+        public IRelayCommand AddCedulePointageCommand { get; }
+        public IRelayCommand SetHeureDebutNowCommand { get; }
+        public IRelayCommand SetHeureFinNowCommand { get; }
+
         public ClockingViewModel(Employee employee, ICeduleQuartService ceduleQuartService)
         {
             _ceduleQuartService = ceduleQuartService;
             Employee = employee;
+            AddCedulePointageCommand = new RelayCommand(async () => await AddCedulePointageAsync());
+            SetHeureDebutNowCommand = new RelayCommand(SetHeureDebutNow);
+            SetHeureFinNowCommand = new RelayCommand(SetHeureFinNow);
             _ = LoadMyShiftToClock(employee);
         }
 
-        // ajouter filtre date?
         public async Task LoadMyShiftToClock(Employee employee)
         {
             var shiftList = await _ceduleQuartService.GetQuartListByEmployeeId(employee.Id);
             ShiftToClockList = new ObservableCollection<CeduleQuart>(shiftList);
+        }
+
+        private async Task AddCedulePointageAsync()
+        {
+            if (NewHeureDebut == null || NewHeureFin == null)
+            {
+                return;
+            }
+
+            if (NewHeureFin <= NewHeureDebut)
+            {
+                
+                return;
+            }
+
+            var newCedule = new CeduleQuart
+            {
+                IdEmployee = Employee.Id,
+                IdCreateur = Employee.Id, 
+                TypeQuart = 1, 
+                IsPausePayee = false,
+                HeureDebut = NewHeureDebut.Value,
+                HeureFin = NewHeureFin.Value,
+                IsPointageApprouve = false
+            };
+
+            try
+            {
+                await _ceduleQuartService.AddCeduleQuartAsync(newCedule);
+                await LoadMyShiftToClock(Employee);
+
+               
+                NewHeureDebut = null;
+                NewHeureFin = null;
+            }
+            catch (Exception ex)
+            {
+                
+            }
+        }
+
+        private void SetHeureDebutNow()
+        {
+            NewHeureDebut = DateTime.Now;
+        }
+
+        private void SetHeureFinNow()
+        {
+            NewHeureFin = DateTime.Now;
         }
     }
 }
